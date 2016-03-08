71cc37495d9f4c78ca8797c21e4bb158b17a88c5
david szyszkowski <try2murkme@gmail.com>
2016-03-08 08:37:52 -0800

    made new git cheat

---
+++ wdi/cheatsheets/git_cheatsheets.js
@@ -0,0 +1,23 @@
+github protocols
+
+on github
+make new or use existing repo
+
+terminal
+cd to code/wdi
+mkdir       //local folder
+cd to that directory
+and then touch file    //local file
+git init
+git status. nothing to commit
+git add file
+git commit -m "add file"
+git remote -v. if nothing happens   // trouble shooting
+git remote add origin /* copy repo link here */
+git remote -v //should show a fetch and pull
+git push origin master. error, then do:
+git pull origin
+git push origin master
+git pull origin master
+git push origin master
+check github in that repo
