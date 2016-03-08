213c6d0840d64a2439eb9ef01c5a3874b162c22f
david szyszkowski <try2murkme@gmail.com>
2016-03-08 08:46:59 -0800

    added o command line

---
+++ wdi/cheatsheets/bash_cheatsheets.js
@@ -0,0 +1,16 @@
+command line
+pry //ruby program //type ruby commands
+command+m // new window
+subl .//open up sublime current folder
+. // current directory
+subl code/wdi //opens this folder
+delist //linting program to find errors with scripting language
+option + click //to go to specific spot in line
+ls -la
+ls -l
+ls -a
+-p
+pwd //print working dir
+clear or command +k //clear
+rm -rf //force. don’t use, possible to delete operating system
+b // up one, or back
